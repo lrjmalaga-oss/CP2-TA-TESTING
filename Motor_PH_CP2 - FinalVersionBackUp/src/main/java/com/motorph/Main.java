0b8cb42a9585b748d98bb266fb1b0cb63f9f25e8
lrjmalaga-oss <lr.jmalaga@mmdc.mcl.edu.ph>
2026-07-10 15:20:19 +0300

    Update Main.java
    Comments added.

--- Motor_PH_CP2 - FinalVersionBackUp/src/main/java/com/motorph/Main.java
+++ Motor_PH_CP2 - FinalVersionBackUp/src/main/java/com/motorph/Main.java
@@ -3,14 +3,31 @@ package com.motorph;
 import com.motorph.gui.*;
 
 import javax.swing.*;
-import java.awt.*; 
+import java.awt.*;
 import java.awt.event.*;
 
- public class Main { 
- 
- public static void main(String[] args) {
+/**
+ * Main class of the MotorPH Employee Application.
+ *
+ * This class serves as the entry point of the application.
+ * It initializes and launches the main graphical user interface (GUI)
+ * by creating an instance of the MainFrame class.
+ *
+ * @author Group 1
+ * @version Milestone 2
+ */
+public class Main {
 
- new MainFrame();   
-    
- } 
- }
+    /**
+     * Application entry point.
+     *
+     * Creates and displays the application's main window.
+     *
+     * @param args Command-line arguments (not used).
+     */
+    public static void main(String[] args) {
+
+        // Launch the main application window.
+        new MainFrame();
+    }
+}
